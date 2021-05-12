2d1ab361cfbf5be792fc80fc0b1b2ca600b69507
qrustik <rusteamplay@gmail.com>
2021-05-12 11:10:10 +0500

    asd

--- source/kth-element.hpp
+++ source/kth-element.hpp
@@ -5,7 +5,9 @@ namespace myalgo
 {
     template <typename T>
     T kth_element(T a[], const size_t& k, const size_t& left, const size_t& right) {
-        if ((right - left + 1) == 1) return a[0];
+        if ((right - left + 1) == 1) {
+            return a[0];
+        }
 
         size_t lenghtPart = detail::partition(a, left, right);
         // pivot is l + r / 2
